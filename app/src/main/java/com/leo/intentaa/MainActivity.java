6c545c8d65372e49b93437fa0e41a6223b651ff9
nikhilepparla <e.nikhil1999@gmail.com>
2023-05-31 08:13:43 +0530

    Implicit intent

--- app/src/main/java/com/leo/intentaa/MainActivity.java
+++ app/src/main/java/com/leo/intentaa/MainActivity.java
@@ -1,9 +1,11 @@
 package com.leo.intentaa;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,14 +20,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     Button getButton() {
-        Button button = findViewById(R.id.btn);
+        Button button = findViewById(R.id.button);
         return button;
     }
 
     void onClick(View view) {
         int viewId = view.getId();
-        if (viewId == R.id.btn) {
-            Intent intent = new Intent(MainActivity.this, SecActivity.class);
+        if (viewId == R.id.button) {
+            EditText editText1=findViewById(R.id.edittext);
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:"));
+            intent.putExtra(Intent.EXTRA_EMAIL,new String[]{editText1.getText().toString()});
+            intent.putExtra(Intent.EXTRA_SUBJECT, "I want one day leave");
+            intent.putExtra(Intent.EXTRA_TEXT, "I am having fever");
             startActivity(intent);
         }
     }
